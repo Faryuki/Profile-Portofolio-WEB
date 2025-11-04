03a93381d0ffe6148cf10d279a4e4fca76fc1087
Dhio <dhiozahwan@gmail.com>
2025-11-04 23:34:34 +0700

    Update Foto PP

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -45,7 +45,7 @@ export default function AboutSection() {
             transition={{ duration: 0.8 }}
           >
             <Image
-              src="/profill.png"
+              src="/pp.png"
               alt="Foto Dhio"
               width={280}
               height={280}
